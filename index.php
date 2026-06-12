a56a0d2f94406d14c07aab8d8cd2a86fe065c79b
meharparnami14 <meharparnami14@gmail.com>
2026-06-13 01:20:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
+<?php
+include("db.php");
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $phone = $_POST['phone'];
+
+    $sql = "INSERT INTO patients(name, age, gender, phone)
+            VALUES('$name', '$age', '$gender', '$phone')";
+
+    mysqli_query($conn, $sql);
+
+    echo "<script>alert('Patient Added Successfully!');</script>";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hospital Management System</title>
+    <title>Add Patient</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -13,19 +32,32 @@
 <div class="container">
     <div class="card">
 
-        <h2>Dashboard</h2>
+        <h2>Add Patient</h2>
 
-        <a class="btn" href="add_patient.php">
-            Add Patient
-        </a>
+        <form method="POST">
 
-        <a class="btn" href="view_patients.php">
-            View Patients
-        </a>
+            <label>Name</label>
+            <input type="text" name="name" required>
 
-        <a class="btn" href="search_patient.php">
-            Search Patient
-        </a>
+            <label>Age</label>
+            <input type="number" name="age" required>
+
+            <label>Gender</label>
+            <select name="gender">
+                <option>Male</option>
+                <option>Female</option>
+            </select>
+
+            <label>Phone</label>
+            <input type="text" name="phone" required>
+
+            <input class="btn" type="submit" name="submit" value="Add Patient">
+
+        </form>
+
+        <br>
+
+        <a class="btn" href="index.php">Back Home</a>
 
     </div>
 </div>
