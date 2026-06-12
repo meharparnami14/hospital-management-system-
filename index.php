3b25838c22e01665ec1ade7a23beb723e529eb91
meharparnami14 <meharparnami14@gmail.com>
2026-06-13 00:55:53 +0530

    Add initial HTML structure for hospital management system

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hospital Management System</title>
+</head>
+<body>
+
+    <h1>Hospital Management System</h1>
+
+    <a href="add_patient.php">
+        <button>Add Patient</button>
+    </a>
+
+    <a href="view_patients.php">
+        <button>View Patients</button>
+    </a>
+
+    <a href="search_patient.php">
+        <button>Search Patient</button>
+    </a>
+
+</body>
+</html>
